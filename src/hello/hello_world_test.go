7f10394a2b24891c1df96805be1910befe6b3768
bogdancanciu <bogdancanciu21@gmail.com>
2023-09-26 15:51:34 +0300

    Extract duplicate code into helper function

--- src/hello/hello_world_test.go
+++ src/hello/hello_world_test.go
@@ -6,33 +6,32 @@ func TestGreetWorld(t *testing.T) {
 	got := GreetWorld()
 	want := "Hello, world!"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+	assertCorrectMessage(t, got, want)
 }
 
 func TestGreetPerson(t *testing.T) {
 	got := GreetPerson("John")
 	want := "Hello, John!"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+	assertCorrectMessage(t, got, want)
 }
 
 func TestGreetWithArguments(t *testing.T) {
 	got := Greet("argument")
 	want := "Hello, argument!"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+	assertCorrectMessage(t, got, want)
 }
 
 func TestGreetWithEmptyArgument(t *testing.T) {
 	got := Greet("")
 	want := "Hello, world!"
 
+	assertCorrectMessage(t, got, want)
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
