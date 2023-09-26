5b10906af20e7448e6a01191ef39aef4249434b5
bogdancanciu <bogdancanciu21@gmail.com>
2023-09-26 15:49:54 +0300

    Introduce custom greeter functionality

--- src/hello/hello_world.go
+++ src/hello/hello_world.go
@@ -11,3 +11,13 @@ func GreetWorld() string {
 func GreetPerson(personName string) string {
 	return fmt.Sprintf("%s %s!", greetingPrefix, personName)
 }
+
+func Greet(greeted string) string {
+	result := GreetPerson(greeted)
+
+	if greeted == "" {
+		result = GreetWorld()
+	}
+
+	return result
+}
