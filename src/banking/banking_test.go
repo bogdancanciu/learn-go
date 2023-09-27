0195efa878ef7277b79b32e73e0b52cbba673aab
bogdancanciu <bogdancanciu21@gmail.com>
2023-09-27 15:32:56 +0300

    Refactor unit tests

--- src/banking/banking_test.go
+++ src/banking/banking_test.go
@@ -3,13 +3,24 @@ package banking
 import "testing"
 
 func TestWallet(t *testing.T) {
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
 
-	wallet := Wallet{}
+		assertBalance(t, wallet, Bitcoin(10))
+	})
 
-	wallet.Deposit(Bitcoin(10))
+	t.Run("withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		wallet.Withdraw(Bitcoin(10))
 
-	got := wallet.Balance()
-	want := Bitcoin(10)
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+}
+
+func assertBalance(t *testing.T, w Wallet, want Bitcoin) {
+	t.Helper()
+	got := w.Balance()
 
 	if got != want {
 		t.Errorf("got %s want %s", got, want)
