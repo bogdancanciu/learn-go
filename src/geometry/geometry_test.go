01063a3f72b069ab7288ba63e4a5010d1987b2b0
bogdancanciu <bogdancanciu21@gmail.com>
2023-09-27 14:48:42 +0300

    Refactor geometry tests

--- src/geometry/geometry_test.go
+++ src/geometry/geometry_test.go
@@ -14,18 +14,21 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 	areaTests := []struct {
+		name string
 		shape Shape
-		want  float64
+		hasArea float64
 	}{
-		{Rectangle{12, 6}, 72.0},
-		{Circle{10}, 314.1592653589793},
-		{Triangle{12, 6}, 36.0},
+		{name:"Rectangle", shape: Rectangle{12, 6}, hasArea: 72.0},
+		{name:"Circle", shape: Circle{10}, hasArea: 314.1592653589793},
+		{name:"Triangle", shape: Triangle{12, 6}, hasArea: 36.0},
 	}
 
 	for _, tc := range areaTests {
-		got := tc.shape.Area()
-		if got != tc.want {
-			t.Errorf("got %g want %g", got, tc.want)
-		}
+		t.Run(tc.name, func(t *testing.T){
+			got := tc.shape.Area()
+			if got != tc.hasArea {
+				t.Errorf("got %g want %g", got, tc.hasArea)
+			}
+		})
 	}
 }
