ed3715e57b47e1f3d603dbfc8b1bf154dd29ac8e
bogdancanciu <bogdancanciu21@gmail.com>
2023-10-03 14:13:31 +0300

    Introduce sleeper interface for mocking the delay

--- src/counter/counter.go
+++ src/counter/counter.go
@@ -9,6 +9,18 @@ import (
 const finalWord = "Go!"
 const countdownStart = 3
 
+type Sleeper interface {
+	Sleep()
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
 func Countdown(out io.Writer) {
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
