da9c267dd42da64e0161b78caafb66824ace7ecd
bogdancanciu <bogdancanciu21@gmail.com>
2023-10-03 15:50:55 +0300

    Define benchmark for website checker

--- src/concurrency/concurrency_test.go
+++ src/concurrency/concurrency_test.go
@@ -3,12 +3,18 @@ package concurrency
 import (
 	"reflect"
 	"testing"
+	"time"
 )
 
 func mockWebsiteChecker(url string) bool {
 	if url == "waat://furhurterwe.geds" {
 		return false
 	}
+	return true
+}
+
+func slowStubWebsiteChecker(_ string) bool {
+	time.Sleep(20 * time.Millisecond)
 	return true
 }
 
@@ -29,5 +35,16 @@ func TestCheckWebsites(t *testing.T) {
 
 	if !reflect.DeepEqual(want, got) {
 		t.Fatalf("wanted %v, got %v", want, got)
+	}
+}
+
+func BenchmarkCheckWebsites(b *testing.B) {
+	urls := make([]string, 100)
+	for i := 0; i < len(urls); i++ {
+		urls[i] = "some url"
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		CheckWebsites(slowStubWebsiteChecker, urls)
 	}
 }
