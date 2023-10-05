f94234a4408d6a17a6f04633eb7a46347502394a
bogdancanciu <bogdancanciu21@gmail.com>
2023-10-05 18:11:42 +0300

    Update test case to increment the counter in different goroutines

--- src/syncronize/sync_counter_test.go
+++ src/syncronize/sync_counter_test.go
@@ -1,17 +1,27 @@
 package syncronize
 
 import (
+	"sync"
 	"testing"
 )
 
 func TestCounter(t *testing.T) {
-	t.Run("incrementing the counter 3 times leaves it at 3", func(t *testing.T) {
+	t.Run("it runs safely concurrently", func(t *testing.T) {
+		wantedCount := 1000
 		counter := Counter{}
-		counter.Inc()
-		counter.Inc()
-		counter.Inc()
 
-		assertCounter(t, counter, 3)
+		var wg sync.WaitGroup
+		wg.Add(wantedCount)
+
+		for i := 0; i < wantedCount; i++ {
+			go func() {
+				counter.Inc()
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+
+		assertCounter(t, counter, wantedCount)
 	})
 }
 
